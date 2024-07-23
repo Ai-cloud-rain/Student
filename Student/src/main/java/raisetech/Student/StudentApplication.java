6d68f4624e5eeb8e5eb3bd48f1adffe3917ca75a
Ai-cloud-rain <91471536+Ai-cloud-rain@users.noreply.github.com>
2024-07-23 22:03:28 +0900

    初回コミット

---
+++ Student/src/main/java/raisetech/Student/StudentApplication.java
@@ -0,0 +1,21 @@
+package raisetech.Student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentApplication {
+//localhost:8080
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello, World";
+	}
+
+}
